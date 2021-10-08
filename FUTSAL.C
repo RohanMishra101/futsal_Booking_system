ae738ef0be71062d580ceab71bbbccfc8c507b82
Rohan Mishra <mishrarohan090@gmail.com>
2021-10-08 11:34:08 +0545

    Delay included

--- FUTSAL.C
+++ FUTSAL.C
@@ -6,8 +6,13 @@ void main()
 {
 	int gd = DETECT,gm;
 	initgraph(&gd,&gm,"c:\\TURBOC3\\BGI");
+	delay(500);
 	rectangle(50,80,600,400);
+	delay(500);
 	rectangle(52,82,598,398);
+	delay(500);
+	settextstyle(10,HORIZ_DIR,3);
+	outtextxy(90,90,"Futsal Management System");
 	getch();
 	closegraph();
 
