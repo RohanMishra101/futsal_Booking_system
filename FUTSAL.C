b31fe4a3d94b6cb2a85025f9975b9e9cadf52a94
Rohan Mishra <mishrarohan090@gmail.com>
2021-10-10 12:31:06 +0545

    User interface completed

--- FUTSAL.C
+++ FUTSAL.C
@@ -2,12 +2,13 @@
 #include<conio.h>
 #include<graphics.h>
 void menu();
+void menu_start();
 
 void main()
 {
 	menu();
 }
-
+//UI Interface
 void menu()
 {
 	int gd = DETECT,gm;
@@ -24,7 +25,23 @@ void menu()
 	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 	gotoxy(10,23);
 	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
+	menu_start();
 	getch();
 	closegraph();
+}
+//UI Interface end
+
+//Menu outions
+void menu_start()
+{
+       settextstyle(1,HORIZ_DIR,4);
+       delay(500);
+       gotoxy(30,13);
+       printf("1) Book a Match.\n");
+       gotoxy(30,15);
+       printf("2) Cancel your Booking.\n");
+       gotoxy(30,17);
+       printf("3) View Bookings.\n");
+       gotoxy(30,19);
+       printf("4) Exit.");
 }
